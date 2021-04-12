822ebc6bcd63a3d3e7ce1d3c747a7c545586ea1b
atsalibram <atsalibaram@gmail.com>
2021-04-12 21:33:38 +0300

    update models

--- hood/settings.py
+++ hood/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tinymce',
-    'django_countries'
+    'django_countries',
+    'bootstrap3',
+
 ]
 
 MIDDLEWARE = [
